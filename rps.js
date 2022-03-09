162cfeaefe89f6885364785f54b90aa77c107779
Dylan Palmboom <nofxboy1234@gmail.com>
2022-03-09 12:55:35 +0200

    Add computerPlay function that uses conditional
    Alternative function to calculate the return value of computerPlay using
    a conditional instead of an array.

--- rps.js
+++ rps.js
@@ -9,12 +9,25 @@ Display whether the user won or lost the round
 After 5 rounds, show the user's and computer's final score and the winner
 */
 
-function computerPlay() {
+function computerPlayArray() {
   console.log("computerPlay");
   const choices = ["rock", "paper", "scissors"];
   const randomChoice = Math.floor(Math.random() * choices.length);
 
   return choices[randomChoice];
+}
+
+function computerPlay() {
+  let numberOfChoices = 3;
+  const randomChoice = Math.floor(Math.random() * numberOfChoices);
+
+  if (randomChoice === 0) {
+    return "rock";
+  } else if (randomChoice === 1) {
+    return "paper";
+  } else if (randomChoice === 2) {
+    return "scissors";
+  }
 }
 
 console.log(computerPlay());
