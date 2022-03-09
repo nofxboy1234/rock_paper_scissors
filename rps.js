d37b0a20dcc15880e76d9cd50099f9abdb477c19
Dylan Palmboom <nofxboy1234@gmail.com>
2022-03-09 22:51:05 +0200

    Show score as game continues

--- rps.js
+++ rps.js
@@ -28,11 +28,10 @@ function computerPlay() {
 }
 
 function capitalize(str = "") {
-  return str[0].toUpperCase() + str.slice(1);
+  return str[0].toUpperCase() + str.toLowerCase().slice(1);
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = capitalize(playerSelection);
   let result;
   if (playerSelection === "Rock") {
     if (computerSelection === "Rock") {
@@ -63,16 +62,43 @@ function playRound(playerSelection, computerSelection) {
   return result;
 }
 
+function isPlayerSelectionValid(playerSelection) {
+  if (
+    playerSelection === "Rock" ||
+    playerSelection === "Paper" ||
+    playerSelection === "Scissors"
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function game() {
   let rounds = 5;
   let playerWins = 0;
   let computerWins = 0;
+  let ties = 0;
 
   for (let i = 0; i < rounds; i++) {
-    const playerSelection = prompt(
-      "Welcome to Rock, Paper, Scissors\nWhat is your choice?",
-      "Rock"
-    );
+    let playerSelection = "";
+    while (!isPlayerSelectionValid(playerSelection)) {
+      playerSelection = prompt(
+        "Welcome to Rock, Paper, Scissors\nWhat is your choice?",
+        "Rock"
+      );
+      if (!playerSelection) {
+        console.log("Game cancelled!");
+        return;
+      }
+
+      playerSelection = capitalize(playerSelection);
+
+      if (!isPlayerSelectionValid(playerSelection)) {
+        console.log("Please choose between: Rock, Paper, Scissors");
+      }
+    }
+
     const computerSelection = computerPlay();
     let result = playRound(playerSelection, computerSelection);
 
@@ -85,13 +111,18 @@ function game() {
       computerWins += 1;
     } else if (result === "Tie") {
       resultString = `You ${result}! ${playerSelection} ties with ${computerSelection}`;
+      ties += 1;
     }
+    console.log(
+      `You chose ${playerSelection}, Computer chose ${computerSelection}`
+    );
     console.log(resultString);
+    console.log(
+      `Player wins - ${playerWins}, Computer wins - ${computerWins}, Ties - ${ties}`
+    );
+    console.log(`${rounds - (i + 1)} rounds left`);
   }
 
-  console.log(
-    `Final results: Player wins - ${playerWins}, Computer wins - ${computerWins}`
-  );
   if (playerWins > computerWins) {
     console.log("You win!");
   } else if (computerWins > playerWins) {
