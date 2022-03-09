24a7ddf077df7520b72b7e8294365cf4b5637762
Dylan Palmboom <nofxboy1234@gmail.com>
2022-03-09 12:49:49 +0200

    Add computerPlay() function

--- rps.js
+++ rps.js
@@ -1,1 +1,20 @@
-let a = 2;
+console.log("Rock Paper Scissors!");
+
+/*
+
+When the user inputs "rock", "paper", or "scissors"
+Get the computer's choice of "rock", "paper", or "scissors"
+Compare the user's choice against the computer's choice
+Display whether the user won or lost the round
+After 5 rounds, show the user's and computer's final score and the winner
+*/
+
+function computerPlay() {
+  console.log("computerPlay");
+  const choices = ["rock", "paper", "scissors"];
+  const randomChoice = Math.floor(Math.random() * choices.length);
+
+  return choices[randomChoice];
+}
+
+console.log(computerPlay());
